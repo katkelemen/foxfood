7961b68ff6d72f4ebd06a092acae05c6031b85cc
Kelemen Kata <kelemenkata@gmail.com>
2015-01-08 17:58:07 +0100

    models appear in admin

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from recipe.models import *
+
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(RecipeIngredient)
--- recipe/models.py
+++ recipe/models.py
@@ -4,12 +4,19 @@ from django.db import models
 
 class Recipe(models.Model):
     name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
     
 class Ingredient(models.Model):
     name = models.CharField(max_length=200)
     SI = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.name
 
 class RecipeIngredient(models.Model):
     amount = models.IntegerField(default = 1)
     ingredient = models.ForeignKey(Ingredient)
     recipe = models.ForeignKey(Recipe)
+    def __unicode__(self):
+        return self.recipe.name + " " + self.ingredient.name
